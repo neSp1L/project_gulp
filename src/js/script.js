92c51b10402e26a0c147865b5c8dd8164f728b3d
neSp1L <sasha20.07.asus@gmail.com>
2023-04-06 04:53:44 +0300

    slick

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__iner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: <button type="button" className="slick-prev">Previous</button>
+    });
+});
