cbdc9a245301bb39233c3a3e4c9622479cba69bc
neSp1L <sasha20.07.asus@gmail.com>
2023-04-06 10:48:07 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
             }
         ]
     });
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -33,4 +34,3 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 });
-
